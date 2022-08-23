b535e2a273a20774ad9729e7f65bb09d582b8395
miciav <michele.ciavotta@gmail.com>
2022-08-23 16:23:54 +0200

    Exercise01 improved docker compilation scripts.

--- Exercise01/Docker/compile-multibuild.sh
+++ Exercise01/Docker/compile-multibuild.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker build --no-cache -f Dockerfile-multibuild -t gunicorn-flask-alpine:latest ../
+docker scan gunicorn-flask-alpine:latest
+docker images gunicorn-flask-alpine:latest
--- Exercise01/Docker/compile.sh
+++ Exercise01/Docker/compile.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker build --no-cache -f Dockerfile -t gunicorn-flask:latest ../
+docker scan gunicorn-flask:latest
+docker images gunicorn-flask:latest
